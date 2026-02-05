194179a2ffe5f96a9ca1e25fc69bbc60328df264
Hisyamkamil <hisyamkamilchandrazaman@gmail.com>
2026-02-05 16:59:04 +0700

    Update Module.js

--- dashpanel/Module.js
+++ dashpanel/Module.js
@@ -1,12 +1,12 @@
-Ext.define('Store.SensorMonitoring.Module', {
+Ext.define('Store.dashpanel.Module', {
     extend: 'Ext.Component',
 
     initModule: function () {
         // Create navigation tab component
-        var navTab = Ext.create('Store.SensorMonitoring.view.Navigation');
+        var navTab = Ext.create('Store.dashpanel.view.Navigation');
         
         // Create main panel component for sensor data display
-        var mainPanel = Ext.create('Store.SensorMonitoring.view.MainPanel');
+        var mainPanel = Ext.create('Store.dashpanel.view.MainPanel');
         
         // Link navigation tab to main panel (mandatory requirement)
         navTab.map_frame = mainPanel;
